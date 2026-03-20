2c0c433060b919d9b67b29dc9dc9bef975a70145
kidiee558 <kidiee558@gmail.com>
2026-03-20 13:51:57 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,30 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { schemas } from './schemas';
+import { Iframe } from 'sanity-plugin-iframe-pane';
 
 export default defineConfig({
   name: 'default',
   title: 'PLAN Street Food CMS',
-  projectId: 'xcwdwmij',
+  projectId: 'xcwdwmij', // Twoje poprawne ID
   dataset: 'production',
   basePath: '/admin',
-  plugins: [structureTool()],
+  plugins: [
+    structureTool({
+      defaultDocumentNode: (S, { schemaType }) => {
+        if (schemaType === 'homepage') {
+          return S.document().views([
+            S.view.form().title('Edycja treści'),
+            S.view.component(Iframe).options({
+              url: 'https://plan-stritfud.pages.dev/',
+              reload: { button: true }
+            }).title('Podgląd na żywo')
+          ]);
+        }
+        return S.document().views([S.view.form()]);
+      }
+    }),
+  ],
   schema: {
     types: schemas,
   },
