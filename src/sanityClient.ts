46b2f67f012b29bd38a3dbf20c28a318b40eb8a1
kidiee558 <kidiee558@gmail.com>
2026-03-20 14:44:53 +0100

    Delete src directory

--- src/sanityClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const client = createClient({
-  projectId: 'xcwdwmij', // Twoje ID
-  dataset: 'production',
-  useCdn: false,
-  apiVersion: '2026-03-20',
-});
