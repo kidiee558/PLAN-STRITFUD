f35fc5fb35e6cff7a8f4f354b5ca5e0f618d376c
kidiee558 <kidiee558@gmail.com>
2026-03-21 15:56:03 +0100

    Delete get_menu.cjs

--- get_menu.cjs
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const output = execSync('git show HEAD~1:src/components/Menu.tsx').toString();
-  console.log(output);
-} catch (e) {
-  console.error(e.message);
-}
